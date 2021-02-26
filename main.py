0a2157eda214f0d3abd5954e6f5a5a6b655c84ac
Nassim <nassimsaboundji@gmail.com>
2021-02-26 15:44:18 -0500

    started working on visualizing the data

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ def main():
    
    #example of usage 
 
-   economie = Curriculum('economie','https://admission.umontreal.ca/programmes/baccalaureat-en-sciences-economiques/structure-du-programme/')
-   #economie.download_html() only once should run this
-   soup = economie.soupify()
-   #dl_course_pages_UdeM(soup,'economie') also this only once
+   informatique = Curriculum('informatique','https://admission.umontreal.ca/programmes/baccalaureat-en-informatique/structure-du-programme/')
+   informatique.download_html() #you should only run this once or when you want up to date data
+   soup = informatique.soupify()
+   dl_course_pages_UdeM(soup,'informatique') #also this 
 
    #find all the files in the specified directory
-   file_list = glob.glob("economie/*.html")
+   file_list = glob.glob("informatique/*.html")
 
    for file in file_list:
        schema = course_schema_array(file)
-       economie.add_course(schema)
+       informatique.add_course(schema)
    
-   economie.write_to_JSON('economie')
+   informatique.write_to_JSON('informatique')
 
 if __name__ == '__main__':
     main()   
---
+++ makeCurriculumMap.js
@@ -0,0 +1,53 @@
+
+fetch("./informatique.json") // only change this to generate a new map for another discipline
+.then(response => {
+       return response.json(); 
+})
+.then(data => {
+    keys = Object.keys(data);
+    console.log(keys);
+
+    let dataSet = [];
+    for(let i = 0; i < keys.length; i++){
+        let extraInfo = data[keys[i]]["name"]; //+ "\n" + data[keys[i]]["description"]
+        //set the id inside the data object so we can trace the edges between courses
+        data[keys[i]]["id"] = i;
+        dataSet.push({id: data[keys[i]]["id"], label: keys[i], title: extraInfo})
+    }
+
+
+    let nodes = new vis.DataSet(dataSet);
+
+    let edgeData = findEdges(keys, data);
+    let edges =  new vis.DataSet(edgeData);
+
+    let container = document.getElementById('mynetwork');
+
+    let graphData = {
+        nodes: nodes,
+        edges: edges
+    }
+
+    let options = {};
+
+    let network =  new vis.Network(container, graphData, options);
+})
+
+
+function findEdges(keys, data){
+    let edges = [];
+    for(let i = 0; i < keys.length; i++){
+        let prereqsArray = data[keys[i]]["prereqs"];
+        if (prereqsArray == 0 || prereqsArray.length == 0){
+            continue
+        } 
+        for(let j = 0; j < prereqsArray.length; j++){
+            if(data[prereqsArray[j]]){ // if the course exists and is not obsolete
+                edges.push({from: data[prereqsArray[j]]["id"], to: data[keys[i]]["id"]})
+            }
+        }
+    }
+
+    console.log(edges);
+    return edges;
+}
