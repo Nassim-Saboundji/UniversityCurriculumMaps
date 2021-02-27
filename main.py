db4e84dce9148f1ac3b3959e7780c77e3dc2f10f
Nassim <nassimsaboundji@gmail.com>
2021-02-26 22:20:27 -0500

    je me suis trompé avec les arg v

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import sys
 
 def main():
    
-   discipline = Curriculum(sys.argv[0], sys.argv[1])
+   discipline = Curriculum(sys.argv[1], sys.argv[2])
    discipline.download_html() #you should only run this once or when you want up to date data
    soup = discipline.soupify()
-   dl_course_pages_UdeM(soup, sys.argv[0]) #also this 
+   dl_course_pages_UdeM(soup, sys.argv[1]) #also this 
 
    #find all the files in the specified directory
-   file_list = glob.glob(sys.argv[0] + "/*.html")
+   file_list = glob.glob(sys.argv[1] + "/*.html")
 
    for file in file_list:
        schema = course_schema_array(file)
        discipline.add_course(schema)
    
-   discipline.write_to_JSON(sys.argv[0])
+   discipline.write_to_JSON(sys.argv[1])
 
 
 if __name__ == '__main__':
